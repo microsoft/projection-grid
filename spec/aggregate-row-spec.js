e64592d2625e3d39e762aa4d2639d5b1cea898cf
Wei Wei <lyweiwei@outlook.com>
2016-04-14 00:45:26 +0800

    fix eslint errors in spec files

--- spec/aggregate-row-spec.js
+++ spec/aggregate-row-spec.js
@@ -1,43 +1,39 @@
 var expect = require('chai').expect;
-var sinon = require('sinon');
 var AggregateRow = require('component/grid/projection/aggregate-row');
 var Base = require('component/grid/projection/base');
-var Response = require( 'component/grid/model/response');
+var Response = require('component/grid/model/response');
 
 describe('projection AggregateRow', function () {
-
-  it('update should run normal', function() {
+  it('update should run normal', function () {
     var model = new AggregateRow({
-      'aggregate.top': function(data) {
+      'aggregate.top': function (data) {
         return [{
           name: data.get('value').length,
-          $metadata: {}
+          $metadata: {},
         }];
       },
 
-      'aggregate.bottom': function(data) {
+      'aggregate.bottom': function (data) {
         return [{
           name: data.get('value').length,
-          $metadata: {} 
+          $metadata: {},
         }];
-      }
+      },
     });
 
     var originalData = new Base();
     originalData.data = new Response({
       value: [
-        {name: new String('hello'), id: 'hello_id'},
-        {name: new String('world'), id: 'world_id'}
-      ]
+        { name: 'hello', id: 'hello_id' },
+        { name: 'world', id: 'world_id' },
+      ],
     });
 
     originalData.pipe(model);
-    expect(model.data.get('value')[0]['$metadata']).to.be.eql({type: 'aggregate'});
-    expect(model.data.get('value')[0]['name']).to.be.eql(2);
+    expect(model.data.get('value')[0].$metadata).to.be.eql({ type: 'aggregate' });
+    expect(model.data.get('value')[0].name).to.be.eql(2);
 
-    expect(model.data.get('value')[3]['$metadata']).to.be.eql({type: 'aggregate'});
-    expect(model.data.get('value')[3]['name']).to.be.eql(2);
-
+    expect(model.data.get('value')[3].$metadata).to.be.eql({ type: 'aggregate' });
+    expect(model.data.get('value')[3].name).to.be.eql(2);
   });
-
 });
