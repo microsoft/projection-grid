09e28b16561d4e351eed3f5ccb99b227938cc63a
Wei Wei <lyweiwei@outlook.com>
2016-04-19 14:01:59 +0800

    check in the latest built bits

--- dist/projection-grid.js
+++ dist/projection-grid.js
@@ -57,7 +57,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	module.exports = {
 	  GridView: __webpack_require__(1),
 	  projections: __webpack_require__(9),
-	  layout: __webpack_require__(40),
+	  layout: __webpack_require__(42),
 	};
 
 
@@ -305,9 +305,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	  Page: __webpack_require__(33),
 	  PropertyTemplate: __webpack_require__(34),
 	  RowCheckbox: __webpack_require__(35),
-	  RowIndex: __webpack_require__(37),
-	  Sink: __webpack_require__(38),
-	  ColumnGroup: __webpack_require__(39),
+	  RowTriStateCheckboxProjection: __webpack_require__(37),
+	  RowIndex: __webpack_require__(39),
+	  Sink: __webpack_require__(40),
+	  ColumnGroup: __webpack_require__(41),
 	};
 
 
@@ -2145,6 +2146,304 @@ return /******/ (function(modules) { // webpackBootstrap
 	  __webpack_require__(2),
 	  __webpack_require__(3),
 	  __webpack_require__(11),
+	  __webpack_require__(38),
+	], __WEBPACK_AMD_DEFINE_RESULT__ = function (_, Backbone, BaseProjection, defaultRowCheckTemp) {
+	  'use strict';
+	
+	  var CheckTransitionRule = {};
+	
+	  // CheckTransitionRule functions determine the state transition of a checkbox.
+	  // State transition depends on the current state and wheather a short or full cycle is requested.
+	  // When the short  cycle is requested the checkbox cannot return to the indeterminate state,
+	  // event if it started in this state.
+	  // Two directions of transition are available:
+	  // ... -> indeterminate -> checked -> unchecked -> ...
+	  // ... -> indeterminate -> unchecked -> checked -> ...
+	  CheckTransitionRule.indeterminateToChecked = (function () {
+	    var nextState = {
+	      unchecked: function (shortCycle) {
+	        return shortCycle ? 'checked' : 'indeterminate';
+	      },
+	      checked: _.constant('unchecked'),
+	      indeterminate: _.constant('checked'),
+	    };
+	
+	    return function (shortCycle, currentState) {
+	      return _.has(nextState, currentState) && nextState[currentState](shortCycle) || 'unchecked';
+	    };
+	  })();
+	
+	  CheckTransitionRule.indeterminateToUnchecked = (function () {
+	    var nextState = {
+	      unchecked: _.constant('checked'),
+	      checked: function (shortCycle) {
+	        return shortCycle ? 'unchecked' : 'indeterminate';
+	      },
+	      indeterminate: _.constant('unchecked'),
+	    };
+	
+	    return function (shortCycle, currentState) {
+	      return _.has(nextState, currentState) && nextState[currentState](shortCycle) || 'unchecked';
+	    };
+	  })();
+	
+	  CheckTransitionRule.indeterminateToCheckedFullCycle = function (currentState) {
+	    return CheckTransitionRule.indeterminateToChecked(false, currentState);
+	  };
+	
+	  CheckTransitionRule.indeterminateToUncheckedFullCycle = function (currentState) {
+	    return CheckTransitionRule.indeterminateToUnchecked(false, currentState);
+	  };
+	
+	  CheckTransitionRule.indeterminateToCheckedShortCycle = function (currentState) {
+	    return CheckTransitionRule.indeterminateToChecked(true, currentState);
+	  };
+	
+	  CheckTransitionRule.indeterminateToUncheckedShortCycle = function (currentState) {
+	    return CheckTransitionRule.indeterminateToUnchecked(true, currentState);
+	  };
+	
+	  function getAllCheckState(checkStateCounters, count) {
+	    return _.findKey(checkStateCounters, function (c) {
+	      return c === count;
+	    }) || 'unchecked';
+	  }
+	
+	  var Model = BaseProjection.extend({
+	    defaults: {
+	      'column.checked': 'checkbox',  // the checkbox column
+	      'row.check.id': 'Id',
+	      'row.check.map': {},
+	      'row.check.all': { state: 'unchecked' },    // used to store user's check value for the special case no rows or all rows is disabled
+	      'row.check.allow': function () {
+	        return true;
+	      },
+	      'row.check.click-anywhere': false,
+	      'row.check.transition': CheckTransitionRule.indeterminateToCheckedShortCycle,
+	      'row.check.all.transition': CheckTransitionRule.indeterminateToCheckedShortCycle,
+	      'row.check.template': defaultRowCheckTemp,
+	    },
+	    name: 'row-check',
+	    events: {
+	      'layout:click:cell': 'tdClick',
+	      'layout:click:header': 'thClick',
+	    },
+	    reset: function () {
+	      this.set({
+	        'row.check.checked.all': false,
+	        'row.check.map': {},
+	      });
+	    },
+	    update: function (options) {
+	      if (!Model.__super__.update.call(this, options)) {
+	        return;
+	      }
+	
+	      var checkId = this.get('row.check.id');
+	      var ids = _.pluck(this.src.data.get('value'), checkId);
+	      var checkMap = this.get('row.check.map');
+	      var col = this.get('column.checked');
+	      var columns = _.clone(this.src.data.get('columns'));
+	      var checkStateCounters = _.object(
+	          ['unchecked', 'checked', 'indeterminate'],
+	          [0, 0, 0]
+	        );
+	      var hasCheckboxable = false;
+	      var checkboxAllow = this.get('row.check.allow');
+	      var checkboxColumn = _.find(columns, function (item) {
+	        return item.property === col;
+	      });
+	      var rowCheckTemp = this.get('row.check.template');
+	
+	      var value = _.map(this.src.data.get('value'), function (item) {
+	        var ret = _.clone(item);
+	        var check = checkMap[ret[checkId]] || { state: 'unchecked' };
+	        var disabled = true;
+	        var isAllowed = _.isFunction(checkboxAllow) ? checkboxAllow(ret) : checkboxAllow;
+	
+	        checkStateCounters[check.state]++;
+	
+	        if (isAllowed) {
+	          disabled = false;
+	          hasCheckboxable = true;
+	        }
+	
+	        ret[col] = _.extend({}, ret[col], {
+	          $html: rowCheckTemp({ checkState: check.state, disabled: disabled }),
+	        });
+	
+	        return ret;
+	      });
+	
+	      // set the checkbox in th
+	      if (!_.isUndefined(checkboxColumn)) {
+	        var disabledAllCheck = _.size(ids) === 0;
+	
+	        if (hasCheckboxable) {
+	          var checkState = getAllCheckState(checkStateCounters, ids.length);
+	
+	          checkboxColumn.$html = rowCheckTemp({ checkState: checkState, disabled: disabledAllCheck });
+	          this.attributes['row.check.all'] = _.extend(this.attributes['row.check.all'], { state: checkState });
+	        } else {
+	          checkboxColumn.$html = rowCheckTemp({ checkState: this.get('row.check.all').state, disabled: disabledAllCheck });
+	        }
+	      }
+	
+	      this.patch({
+	        value: value,
+	        columns: columns,
+	      });
+	    },
+	    tdClick: function (e, arg) {
+	      var checkboxProperty = this.get('column.checked');
+	      var clickAnywhere = this.get('row.check.click-anywhere');
+	
+	      if (arg.property === checkboxProperty || clickAnywhere) {
+	        var checkMap = _.clone(this.get('row.check.map'));
+	        var id = arg.model[this.get('row.check.id')];
+	        var defaultTransition = this.get('row.check.transition');
+	        var check = _.extend({ transition: defaultTransition, state: 'unchecked' }, checkMap[id]);
+	
+	        check.state = check.transition(check.state);
+	        checkMap[id] = check;
+	
+	        this.set({
+	          'row.check.map': checkMap,
+	        });
+	      }
+	    },
+	    thClick: function (e, arg) {
+	      var checkboxProperty = this.get('column.checked');
+	
+	      if (arg.property === checkboxProperty) {
+	        var checkMap = _.clone(this.get('row.check.map'));
+	        var checkId = this.get('row.check.id');
+	        var allCheck = this.get('row.check.all');
+	        var checkState = allCheck.state;
+	        var allCheckTransitionRule = this.get('row.check.all.transition');
+	        var CheckTransitionRule = this.get('row.check.transition');
+	
+	        allCheck.state = allCheckTransitionRule(checkState);
+	
+	        checkMap = _.object(
+	          this.data.get('value')
+	          .map(function (item) {
+	            var check = _.extend({ transition: CheckTransitionRule }, checkMap[item[checkId]], { state: allCheck.state });
+	
+	            return [item[checkId], check];
+	          })
+	        );
+	
+	        this.set({
+	          'row.check.map': checkMap,
+	          'row.check.all': allCheck,
+	        });
+	      }
+	    },
+	  });
+	
+	  function CheckDiff(added, changed, removed, unchanged) {
+	    this.added = added || {};
+	    this.changed = changed || {};
+	    this.removed = removed || {};
+	    this.unchanged = unchanged || {};
+	  }
+	
+	  CheckDiff.prototype.hasChanges = function () {
+	    return _.keys(this.changed).length + _.keys(this.added).length > 0;
+	  };
+	
+	  function diffCheckMap(before, after, defaultState) {
+	    defaultState = defaultState || { state: 'unchecked' };
+	
+	    var checkDiff = new CheckDiff();
+	
+	    _.keys(before).forEach(function (key) {
+	      var beforeState = before[key];
+	      var afterState = after[key];
+	
+	      if (!afterState) {
+	        checkDiff.removed[key] = beforeState;
+	      } else if (beforeState.state === afterState.state) {
+	        checkDiff.unchanged[key] = afterState;
+	      } else {
+	        checkDiff.changed[key] = { before: beforeState, after: afterState };
+	      }
+	    });
+	
+	    _.keys(after).forEach(function (key) {
+	      var afterState = after[key];
+	      var beforeState = before[key];
+	
+	      if (!beforeState && defaultState.state !== afterState.state) {
+	        checkDiff.added[key] = afterState;
+	      }
+	    });
+	
+	    return checkDiff;
+	  }
+	
+	  function statCheckMap(checkMap, defaultState) {
+	    defaultState = defaultState || { state: 'unchecked' };
+	    var checked = [];
+	    var indeterminate = [];
+	    var unchecked = [];
+	
+	    _.keys(checkMap).forEach(function (key) {
+	      var check = checkMap[key];
+	
+	      if (check.state === 'checked' && check.state !== defaultState.state) {
+	        checked.push(check);
+	      } else if (check.state === 'unchecked' && check.state !== defaultState.state) {
+	        unchecked.push(check);
+	      } else if (check.state !== defaultState.state) {
+	        indeterminate.push(check);
+	      }
+	    });
+	
+	    return {
+	      checked: checked,
+	      unchecked: unchecked,
+	      indeterminate: indeterminate,
+	    };
+	  }
+	
+	  function fullStatCheckMap(before, after, defaultState) {
+	    return _.extend(statCheckMap(after, defaultState), diffCheckMap(before, after, defaultState));
+	  }
+	
+	  Model.CheckTransitionRule = CheckTransitionRule;
+	  Model.fullStatCheckMap = fullStatCheckMap;
+	  Model.statCheckMap = statCheckMap;
+	  Model.diffCheckMap = diffCheckMap;
+	
+	  return Model;
+	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+
+
+/***/ },
+/* 38 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var jade = __webpack_require__(20);
+	
+	module.exports = function template(locals) {
+	var buf = [];
+	var jade_mixins = {};
+	var jade_interp;
+	;var locals_for_with = (locals || {});(function (checkState) {
+	var classes = [].concat(checkState === 'indeterminate' ? ['glyphicon', 'glyphicon-minus'] : checkState === 'checked' ? ['glyphicon', 'glyphicon-ok'] : [])
+	buf.push("<div class=\"column-tri-state-checkbox\"><span" + (jade.cls([classes], [true])) + "></span></div>");}.call(this,"checkState" in locals_for_with?locals_for_with.checkState:typeof checkState!=="undefined"?checkState:undefined));;return buf.join("");
+	}
+
+/***/ },
+/* 39 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
+	  __webpack_require__(2),
+	  __webpack_require__(3),
+	  __webpack_require__(11),
 	  __webpack_require__(14),
 	  __webpack_require__(12),
 	], __WEBPACK_AMD_DEFINE_RESULT__ = function (_, Backbone, BaseProjection /* , schemaProperties, Response */) {
@@ -2174,7 +2473,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 38 */
+/* 40 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
@@ -2212,7 +2511,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 39 */
+/* 41 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
@@ -2303,20 +2602,20 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 40 */
+/* 42 */
 /***/ function(module, exports, __webpack_require__) {
 
 	module.exports = {
-	  TableLayout: __webpack_require__(41),
+	  TableLayout: __webpack_require__(43),
 	  templates: {
-	    table: __webpack_require__(42),
+	    table: __webpack_require__(44),
 	  },
-	  renderers: __webpack_require__(43),
+	  renderers: __webpack_require__(45),
 	};
 
 
 /***/ },
-/* 41 */
+/* 43 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
@@ -2587,7 +2886,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 42 */
+/* 44 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var jade = __webpack_require__(20);
@@ -2870,24 +3169,24 @@ return /******/ (function(modules) { // webpackBootstrap
 	}
 
 /***/ },
-/* 43 */
+/* 45 */
 /***/ function(module, exports, __webpack_require__) {
 
 	module.exports = {
-	  FixedHeader: __webpack_require__(44),
-	  Virtualization: __webpack_require__(47),
+	  FixedHeader: __webpack_require__(46),
+	  Virtualization: __webpack_require__(49),
 	};
 
 
 /***/ },
-/* 44 */
+/* 46 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
 	  __webpack_require__(6),
 	  __webpack_require__(2),
-	  __webpack_require__(45),
-	  __webpack_require__(46),
+	  __webpack_require__(47),
+	  __webpack_require__(48),
 	], __WEBPACK_AMD_DEFINE_RESULT__ = function ($, _, measure, px) {
 	  function Renderer(options) {
 	    this.options = options || {};
@@ -2934,12 +3233,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	      // todo [akamel] [perf] 12% -- consider replacing with css rule generation
 	      // e. freeze column width
 	      // e.1 freeze col width
-	      var colIndex = 0, secondHeadTDIndex = 0;
-	      _.each($target, function (td, index) {
-	        var colspan = parseInt($(td).attr('colspan'));
-	        var rowspan = parseInt($(td).attr('rowspan'));
+	      var colIndex = 0;
+	      var secondHeadTDIndex = 0;
+	      _.each($target, function (td) {
+	        var colspan = parseInt($(td).attr('colspan'), 10);
+	        var rowspan = parseInt($(td).attr('rowspan'), 10);
 	        var width = 0;
-	        for (i = 0; i < colspan; ++i) {
+	        for (var i = 0; i < colspan; ++i) {
 	          var colWidth = px.pixelify(this.colWidth[colIndex + i]);
 	          width += colWidth;
 	          if (rowspan === 1) {
@@ -2984,7 +3284,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 45 */
+/* 47 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
@@ -3069,7 +3369,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 46 */
+/* 48 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;// todo [akamel] move to /component
@@ -3094,7 +3394,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 47 */
+/* 49 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;// TODO [akamel] [bug] with large data set, jitters when scrolling to bottom
@@ -3102,8 +3402,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
 	  __webpack_require__(6),
 	  __webpack_require__(2),
-	  __webpack_require__(45),
-	  __webpack_require__(46),
+	  __webpack_require__(47),
+	  __webpack_require__(48),
 	], __WEBPACK_AMD_DEFINE_RESULT__ = function ($, _, measure, px) {
 	  function rowHeight(row) {
 	    return _.isNumber(row.__height) ? row.__height : this.__measures.avgRowHeight;
