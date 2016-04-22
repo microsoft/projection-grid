26ac64499a2a6647bf060d3e37234b970d907395
Wei Wei (BINGADS) <wewei@microsoft.com>
2016-04-22 18:54:11 +0800

    fix gulp task test:demos for windows

--- demos/webpack.config.js
+++ demos/webpack.config.js
@@ -6,7 +6,7 @@ webpackConfig.plugins = webpackConfig.plugins || [];
 webpackConfig.plugins.push(new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }));
 
 webpackConfig.module.loaders = webpackConfig.module.loaders.concat([
-  { test: /bootstrap[\/\\]js[\/\\]/, loader: 'imports?jQuery=jquery' },
+  { test: /bootstrap[\\\/]js[\\\/]/, loader: 'imports?jQuery=jquery' },
   { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
   { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
   { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
