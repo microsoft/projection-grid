40c3530d52a5c0dde7a0459e7a195285e8846349
Wei Wei <lyweiwei@outlook.com>
2016-04-22 10:09:06 +0800

    add the test:demos case

--- demos/simple/specs/index.js
+++ demos/simple/specs/index.js
@@ -1,10 +1,19 @@
-var chai = require('chai');
-var expect = chai.expect;
+var expect = require('chai').expect;
 
-/* globals browser */
-describe('projection grid', function () {
-  it('should have the given title', function () {
-    var title = browser.getTitle();
-    expect(title).to.equal('demos_simple');
+/* global browser */
+/* eslint no-unused-expressions: 0 */
+describe('webdriver.io page', function () {
+  beforeEach(function (cb) {
+    browser.waitForExist('.table.grid', cb);
+  });
+
+  it('should expand and collapse the group column', function () {
+    var collpaseSpan = browser.element('.pop-collapse');
+    expect(collpaseSpan).to.be.exist;
+    collpaseSpan.click();
+    var expandBtn = browser.element('.pop-expand');
+    expect(expandBtn).to.be.exist;
+    expandBtn.click();
+    expect(browser.isExisting('.pop-collapse')).to.be.true;
   });
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -2,7 +2,12 @@ var path = require('path');
 var democase = require('democase');
 
 var demoSet = democase.loadSync(path.resolve(__dirname, 'demos'));
+var config = demoSet.wdioConfig({
+  // capabilities: [{
+  //   browserName: 'firefox',
+  // }],
+  reporters: ['dot', 'junit'],
+  reporterOptions: { outputDir: './test-results/' },
+});
 
-module.exports = {
-  config: demoSet.wdioConfig(),
-};
+module.exports = { config: config };
