3e089bb7201f11162ab6619bb2e547bb18603605
Wei Wei <lyweiwei@outlook.com>
2016-08-03 16:08:11 +0800

    use duck typed array for bodyRows

--- js/vnext/projection/rows.js
+++ js/vnext/projection/rows.js
@@ -5,10 +5,10 @@ export function rows(state, {
   footRows = [],
 } = {}) {
   const columns = state.columns;
-  const bodyRows = _.map(state.items, item => ({
-    classes: ['body-row'],
-    item, 
-  })); 
+  const bodyRows = {
+    length: state.items.length,
+    slice: (...args) => state.items.slice(...args).map(item => ({ item })),
+  };
   return _.defaults({ headRows, bodyRows, footRows }, state);
 }
 
