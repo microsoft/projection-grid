7700139ae6c806f8aebf13532b32a18d064404c4
Shulan Pan (PERSON CONSULTING) <t-shpan@microsoft.com>
2016-09-06 13:15:06 +0800

    last runable version before moving to plugin

--- js/vnext/projection/editable-option.js
+++ js/vnext/projection/editable-option.js
@@ -1,34 +1,10 @@
 import _ from 'underscore';
 import $ from 'jquery';
 import Backbone from 'backbone';
-import optionFooter from './option-footer.jade';
 import editableOptionTemplate from './editable-option.jade';
-
-import './editable-option.less';
-
-const PopupCreate = Backbone.View.extend({
-  events: {
-
-  },
-  initialize: function (options) {
-
-  },
-  render: function () {
-
-  },
-  remove: function () {
-
-  },
-
-});
 
 function deleteRow(editor) {
   return function (e) {
-    /*
-    const $tr = $(e.target).closest('tr');
-    this.trigger('willDelet', null);
-    const primaryKey = $tr.find("td.primaryKey").text();
-    */
     const index = this.indexOfElement(e.target);
     const primaryKey = _.result(this.itemAt(index), this.primaryKey);
     editor.destroy(primaryKey);
