44814c821052f2dc4845be380b57093600f0270c
hezhan <hezhan@microsoft.com>
2016-09-26 04:48:06 -0700

    add type check for accessibility config

--- js/projection/accessibility.js
+++ js/projection/accessibility.js
@@ -3,15 +3,17 @@ define([
   'lib/backbone',
   'component/grid/projection/base',
 ], function (_, Backbone, BaseProjection /* , schemaProperties, Response */) {
+  const defaultPrefix = 'accessibility-';
   var Model = BaseProjection.extend({
     defaults: {
-			'accessibility.rowcheck.idPrefix': 'accessibility-',
+      'accessibility.rowcheck.idPrefix': defaultPrefix,
 		},
 		name: 'accessibility',
 		update: function (options) {
+      var accessibilityPre = this.get('accessibility.rowcheck.idPrefix');
       if (Model.__super__.update.call(this, options)) {
         this.patch({
-          'accessibility.rowcheck.idPrefix': this.get('accessibility.rowcheck.idPrefix'),
+          'accessibility.rowcheck.idPrefix': _.isString(accessibilityPre) ? accessibilityPre : defaultPrefix,
         });
       }
 		}
