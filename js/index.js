ce075d0b32fc0e68a9160f1168821c9124978fd7
Wei Wei (BINGADS) <wewei@microsoft.com>
2016-10-29 05:45:01 +0800

    Update the version number in index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ import {
 } from './vnext/data-source';
 
 module.exports = {
-  version: '0.1.0-43',
+  version: '0.1.0-44',
   GridView: require('./grid-view'),
   projections: require('./projection/index'),
   layout: require('./layout/index'),
