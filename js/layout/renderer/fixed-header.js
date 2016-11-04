e1f7907e531124c44c3c31667ca5110b4ef68d0b
he zhang <hezhan@microsoft.com>
2016-11-04 10:44:29 +0800

    revert change for test

--- js/layout/renderer/fixed-header.js
+++ js/layout/renderer/fixed-header.js
@@ -27,10 +27,7 @@ define([
     // TODO [wewei] this is a hack to temporarily solve the sticky header doesn't
     // respect the navbar problem.
     // We need a better solution on this.
-    var tmp = this.layout.top || 0;
-    var dataHeight = data.vpMeasures.viewportTop + tmp;
-    if (dataHeight > data.vpMeasures.boundsTop) {
-    // if (data.vpMeasures.viewportTop + (this.layout.top || 0) > data.vpMeasures.boundsTop) {
+    if (data.vpMeasures.viewportTop + (this.layout.top || 0) > data.vpMeasures.boundsTop) {
       var $el = this.layout.$el;
 
       newState = 'sticky';
