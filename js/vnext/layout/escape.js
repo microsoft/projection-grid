b814cc3994942787bce4e9671ad812b07e003de6
yaoyao-ict <yaoyao.ict@gmail.com>
2018-12-19 19:33:56 +0800

    move escape logic out of template file (#81)
    * temp
    
    * temp
    
    * fix test
    
    * escape attributes and add item index projection
    
    * add item index projection
    
    * move escape from template to js file
    
    * escape only string

---
+++ js/vnext/layout/escape.js
@@ -0,0 +1,47 @@
+const pugMatch = /["&<>]/;
+
+function pugEscape(_html) {
+  const html = String(_html);
+  const regexResult = pugMatch.exec(html);
+  if (!regexResult) {
+    return _html;
+  }
+
+  let result = '';
+  let i, lastIndex, escape;
+  for (i = regexResult.index, lastIndex = 0; i < html.length; i++) {
+    switch (html.charCodeAt(i)) {
+      case 34: escape = '&quot;'; break;
+      case 38: escape = '&amp;'; break;
+      case 60: escape = '&lt;'; break;
+      case 62: escape = '&gt;'; break;
+      default: continue;
+    }
+    if (lastIndex !== i) {
+      result += html.substring(lastIndex, i);
+    }
+    lastIndex = i + 1;
+    result += escape;
+  }
+  if (lastIndex !== i) {
+    return result + html.substring(lastIndex, i);
+  }
+
+  return result;
+}
+
+export function escapeAttr(attr) {
+  if (Array.isArray(attr)) {
+    return attr.map(function (val) {
+      return escapeAttr(val);
+    });
+  } else if (typeof attr === 'object') {
+    return Object.keys(attr).reduce(function (memo, key) {
+      memo[key] = escapeAttr(attr[key]);
+      return memo;
+    }, {});
+  } else if (typeof attr === 'string') {
+    return pugEscape(attr);
+  }
+  return attr;
+}
